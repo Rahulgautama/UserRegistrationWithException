6adb4f539ea81a547f5938536ed9ee7c0a410a2c
Rahulgautama <rahul8874950727@gmail.com>
2023-05-26 01:59:37 +0530

    First Name validate

---
+++ UserRegistrationProblemWithException/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblemWithException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            Console.WriteLine("Please Enter your first Name With first char cap and min 3 len");
+            try
+            {
+                var firstName = Console.ReadLine();
+                var regexDt = new Regex("^[A-Z][a-zA-Z]{2,20}$");
+
+                if (regexDt.IsMatch(firstName))
+                    Console.WriteLine("Valid First Name");
+                else
+                    Console.WriteLine("Not Valid First Name");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Please Enter Valid Name");
+            }
+        }
+    }
+}
