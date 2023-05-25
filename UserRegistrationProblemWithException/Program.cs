70e3bf813084d268bcab63865f76b71cbb686b6e
Rahulgautama <rahul8874950727@gmail.com>
2023-05-26 02:16:47 +0530

    Password one Upper char validate

--- UserRegistrationProblemWithException/Program.cs
+++ UserRegistrationProblemWithException/Program.cs
@@ -13,7 +13,7 @@ namespace UserRegistrationProblemWithException
             {
                 Console.WriteLine("Please Enter your Password min 8 digit upper");
                 var lastName = Console.ReadLine();
-                var regexDt = new Regex(@"^[a-z]{8,16}$");
+                var regexDt = new Regex(@"^[a-zA-Z]{8,16}$");
 
                 if (regexDt.IsMatch(lastName))
                     Console.WriteLine("Valid Password ");
